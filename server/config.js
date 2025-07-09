aa8f52096491c8bb9ce88ca54c581b22400f4346
printhelloworldasdas <165765404+printhelloworldasdas@users.noreply.github.com>
2025-07-09 17:23:07 +0200

    Create config.js

---
+++ server/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  map: {
+    width: 5000,
+    height: 5000,
+    gridSize: 50
+  },
+
+  game: {
+    mode: "normal",  // Nombre oficial del modo de juego
+    maxPlayers: 100,
+    tickRate: 20
+  },
+
+  player: {
+    radius: 20,
+    speed: 3,
+    health: 100,
+    spawnMargin: 100
+  },
+
+  server: {
+    port: 3000
+  }
+};
