25dd983d4ec835de646c03c26364d0aaca5aec93
printhelloworldasdas <165765404+printhelloworldasdas@users.noreply.github.com>
2025-07-10 15:31:16 +0200

    Update config.js

--- server/config.js
+++ server/config.js
@@ -20,5 +20,37 @@ module.exports = {
 
   server: {
     port: 3000
+  },
+
+  biomes: {
+    snow: {
+      yStart: 0,
+      yEnd: 1500,
+      speedMultiplier: 0.6
+    },
+    desert: {
+      yStart: 3500,
+      yEnd: 5000,
+      speedMultiplier: 1.0
+    },
+    river: {
+      yStart: 2400,
+      yEnd: 2600,
+      speedMultiplier: 0.9,
+      current: {
+        x: 0.8,
+        y: 0
+      }
+    },
+    sandBanks: {
+      yStart: 2200,
+      yEnd: 2400,
+      speedMultiplier: 1.0
+    },
+    sandBanksBottom: {
+      yStart: 2600,
+      yEnd: 2800,
+      speedMultiplier: 1.0
+    }
   }
 };
